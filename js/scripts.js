e686921f8bb30468e853a2454dcac8b3bd3cb8cd
Abeer Khakwani <abeerkhakwani@gmail.com>
2016-05-03 10:58:29 -0700

    add document ready function

--- js/scripts.js
+++ js/scripts.js
@@ -19,28 +19,31 @@ function createCORSRequest(method, url) {
 function getTitle(text) {
   return text.match('<title>(.*)?</title>')[1];
 }
+$( document ).ready(function() {
+    console.log( "ready!" );
 
-// Make the actual CORS request.
-function makeCorsRequest() {
-  // All HTML5 Rocks properties support CORS.
-  var url = 'https://dev-apicalling.pantheonsite.io/wp-json/wp/v2/posts';
+  // Make the actual CORS request.
+  function makeCorsRequest() {
+    // All HTML5 Rocks properties support CORS.
+    var url = 'https://dev-apicalling.pantheonsite.io/wp-json/wp/v2/posts';
 
-  var xhr = createCORSRequest('GET', url);
-  if (!xhr) {
-    alert('CORS not supported');
-    return;
+    var xhr = createCORSRequest('GET', url);
+    if (!xhr) {
+      alert('CORS not supported');
+      return;
+    }
+
+    // Response handlers.
+    xhr.onload = function() {
+      var text = xhr.responseText;
+      var title = getTitle(text);
+      alert('Response from CORS request to ' + url + ': ' + title);
+    };
+
+    xhr.onerror = function() {
+      alert('Woops, there was an error making the request.');
+    };
+
+    xhr.send();
   }
-
-  // Response handlers.
-  xhr.onload = function() {
-    var text = xhr.responseText;
-    var title = getTitle(text);
-    alert('Response from CORS request to ' + url + ': ' + title);
-  };
-
-  xhr.onerror = function() {
-    alert('Woops, there was an error making the request.');
-  };
-
-  xhr.send();
-}
+  });
