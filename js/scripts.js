a18e6afb721bd0e8d275ff87e3eda73d98200b00
Abeer Khakwani <abeerkhakwani@gmail.com>
2016-05-04 09:41:17 -0700

    trying stuff

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,12 @@ jQuery( document ).ready(function( $ ) {
   $.ajax( {
     url: 'https://dev-apicalling.pantheonsite.io/wp-json/wp/v2/posts',
     method: 'GET',
+    headers: {"Access-Control-Allow-Origin": "*"},
+
     beforeSend: function ( xhr ) {
-    xhr.setRequestHeader( 'X-WP-Nonce' );
+      xhr.setRequestHeader( 'X-WP-Nonce' ,'Access-Control-Allow-Origin: *');
     }
   } )
+
 
 });
