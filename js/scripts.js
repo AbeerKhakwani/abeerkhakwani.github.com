61b5bb495348fdc2d2c6a47adbe6802668ae300c
Abeer Khakwani <abeerkhakwani@gmail.com>
2016-05-04 12:32:33 -0700

    adding s to the end to make it https

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 jQuery( document ).ready(function( $ ) {
-  jQuery.get( "http://dev-apicalling.pantheonsite.io//wp-json/wp/v2/posts", function( response ) {
+  jQuery.get( "https://dev-apicalling.pantheonsite.io//wp-json/wp/v2/posts", function( response ) {
       console.log( response );
   } );
-
 });
