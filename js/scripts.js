167b02ece945845748946f9bb986c2cab8790dc0
Abeer Khakwani <abeerkhakwani@gmail.com>
2016-05-04 18:52:12 -0700

    add code to have it print on the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 jQuery( document ).ready(function( $ ) {
-  jQuery.get( "https://dev-apicalling.pantheonsite.io//wp-json/wp/v2/posts", function( response ) {
-      console.log( response );
+  jQuery.get( "http://dev-apicalling.pantheonsite.io//wp-json/wp/v2/posts", function( posts ) {
+    // console.log( posts );
+    posts.forEach(function(post) {
+      console.log(post.title.rendered);
+      console.log(post.content.rendered);
+      console.log(post.date);
+    $(".posts").append("<div class='col-xs-4 col-md-4'>"+post.title.rendered+ post.content.rendered+"</div>")
+
+
+    });
   } );
 });
